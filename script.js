a724f3e5478eceaa53598715436f1bc0e09e7911
Fation Curo <fationcuro06@gmail.com>
2024-01-12 22:07:17 +0100

    Edited page

---
+++ script.js
@@ -0,0 +1,83 @@
+
+        // //Beginner
+        // let name = "Fation";
+        // let lastName = "Curo";
+        // let age = 18;
+        // let yourProfession = "UX UI Designer";
+
+        // console.log(name, lastName, age, yourProfession)
+
+        // //Objects
+        // let aboutMe = {name:'Fation', lastName:'Curo', age:18, yourProfession:'UX UI Designer'}
+        // console.log(aboutMe)
+
+
+        // //Array
+        // let whoIam = ['Fation', 'Curo', 18, 'UX UI Designer']
+        // console.log (whoIam)
+
+
+
+
+
+
+// // Informacion rreth meje 
+// let name = "Fation";
+// let lastName = "Curo";
+// let age = 17;
+// let job = {title:'UX UI Designer', place:'Pogradec, Albania'};
+// let hobbies = ['Sports', 'Cooking', 'Reading'];
+// alert(hobbies[2]);
+// alert(job.place );
+
+// let adultYears;
+// function calculateAdultYears (){
+//    let adultYears = age + 5
+// }
+// console.log(AD)
+
+// calculateAdultYears()
+// console.log(adultYears);
+
+// // // Njoftim per userin (Alert)
+// // let notification = "Click the button below if you are not a robot!"
+// // alert(notification)
+// // let secondNotification = "Click the button below again, to open the page!"
+// // alert(secondNotification)
+
+
+
+
+
+//Learn function and practice it
+
+function aboutMe (name, lastName, age) {
+    console.log( name + ' '  + lastName + ' ' + age)
+}
+aboutMe('Fation', 'Curo', '18 years old');
+
+
+
+
+
+var number1 = 4;
+var number2 = 8;
+
+function solution(n1, n2) {
+    console.log(n1 + n2);
+}
+solution(number1, number2);
+
+
+
+
+var numri1 = 5;
+var numri2 = 3;
+
+function collect(numri1, numri2) {
+    let amount = numri1 + numri2;
+    return amount;
+  }
+
+  let result = collect (5, 3);
+  console.log(result)
